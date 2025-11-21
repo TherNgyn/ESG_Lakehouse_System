5f11eb65d71127fc739058fc2e8655f75e41ff3b
lamdoan204 <lamvip2k4@gmail.com>
2025-11-21 15:13:20 +0700

    Add Airflow + MinIO

---
+++ airflow/dags/orchestrator.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+
+default_args = {
+    'description' : "A DAG to orchestrate data",
+    'start_date': datetime(2025, 10, 20),
+    'catchup' :False,
+}
+
+
+def example():
+    print("this is example task")
+
+dag = DAG(
+    dag_id="ETL_ESG_Data",
+    default_args= default_args,
+    schedule=timedelta(minutes=5)
+)
+
+with dag:
+    task1 = PythonOperator(
+        task_id= 'example',
+        python_callable = example
+    )
